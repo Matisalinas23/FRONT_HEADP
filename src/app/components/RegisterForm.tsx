b63542904729d06e13efc553e800d875f6c8aa60
Matias Salinas <matisalinas1602@gmail.com>
2025-11-28 20:12:00 -0300

    fixing_register_for_clients

--- src/app/components/RegisterForm.tsx
+++ src/app/components/RegisterForm.tsx
@@ -1,5 +1,6 @@
 import { FormikProps } from "formik";
 import AcceptButton from "./AcceptButton";
+import { authStore } from "../store/authStore";
 
 export interface IFormik {
   name: string;
@@ -16,6 +17,8 @@ export interface IFormik {
 }
 
 export const RegisterForm = ({ formik }: { formik: FormikProps<IFormik> }) => {
+  const { logedUser } = authStore(state => state)
+
   return (
     <form onSubmit={formik.handleSubmit} className="w-100 h-fit bg-[var(--darkgray)] py-8 px-16 flex flex-col gap-4">
       <h2 className="font-semibold text-2xl flex justify-center mb-6">Registrate</h2>
@@ -49,25 +52,25 @@ export const RegisterForm = ({ formik }: { formik: FormikProps<IFormik> }) => {
         onChange={formik.handleChange} placeholder="dd/mm/yyyy"
         className="h-8 bg-[var(--gray)] pl-2 text-sm"
       />
-      {formik.values.particularAddress &&
+      {!logedUser &&
         <input
           name="particularAddress" type="text" value={formik.values.particularAddress}
           onChange={formik.handleChange} placeholder="Ingresa tu direccion particular"
           className="h-8 bg-[var(--gray)] pl-2 text-sm"
         />}
-      {formik.values.city &&
+      {!logedUser &&
         <input
           name="city" type="text" value={formik.values.city}
           onChange={formik.handleChange} placeholder="Ingresa tu ciudad"
           className="h-8 bg-[var(--gray)] pl-2 text-sm"
         />}
-      {formik.values.province &&
+      {!logedUser &&
         <input
           name="province" type="text" value={formik.values.province}
           onChange={formik.handleChange} placeholder="Ingresa tu provincia"
           className="h-8 bg-[var(--gray)] pl-2 text-sm"
         />}
-      {formik.values.country &&
+      {!logedUser &&
         <input
           name="country" type="text" value={formik.values.country}
           onChange={formik.handleChange} placeholder="Ingresa tu país"
